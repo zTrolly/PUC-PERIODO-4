6450799d86ac7c394ed28e777b597ecbbea384ba
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2022-08-23 10:44:21 -0300

    colocar o nome e o vretice no args

---
+++ GRAPH THEORY AND COMPUTABILITY/Trabalhos/Representacao-Grafo/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+
+  public static void main(String[] args) {
+
+    String fileName = args[0];
+    int vertice = Integer.parseInt(args[1]);
+    //DEBUG_PRINT_FILE_NAME_AND_VERTICE(fileName, vertice)
+    System.out.println("Arquivo: " + fileName);
+    System.out.println("Vertice: " + vertice);
+
+
+  
+  }
+}
