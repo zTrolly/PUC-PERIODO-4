1b5a8734717df0d16820a385b86d5ff9a07f71c5
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2022-08-31 18:51:51 -0300

    acho que ta certo

---
+++ GRAPH THEORY AND COMPUTABILITY/Trabalhos/Representacao-Grafo/Python/Main.py
@@ -0,0 +1,40 @@
+import sys
+import os.path
+
+# global lists
+listaPredecessores = []
+listaSucessores = []
+
+
+#logica dos predecessores
+def inicializaListaPredecessores(qtdPredecessores):
+    global listaPredecessores
+    listaPredecessores = [] * (qtdPredecessores -1)
+    return listaPredecessores
+
+def verficaPredecessores(predecessor):
+    global listaPredecessores
+    if predecessor in listaPredecessores:
+        return True
+    else:
+        return False
+
+def adicionaPredecessores(vertice, predecessor):
+    if vertice.predecessor == None:
+        vertice.predecessor = predecessor
+        return
+    else:
+        adicionaPredecessores(vertice.predecessor, predecessor)
+
+
+class predecessor:
+    def __init__(self, predecessor, vertice):
+        self.predecessor = predecessor
+        self.vertice = vertice
+
+class sucessor:
+    def __init__(self, sucessor, vertice):
+        self.sucessor = sucessor
+        self.predecessor = vertice
+
+
